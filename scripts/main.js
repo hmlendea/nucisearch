bc70225042f333fe4247faab3eabff6b7936f866
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-04-03 09:55:04 +0300

    rally: Sort matches

--- scripts/main.js
+++ scripts/main.js
@@ -61,7 +61,7 @@ function runSearch(rawQuery) {
     } else {
         const words = query.split(/\s+/);
 
-        if (/^(?:DE|US|F)[0-9]{6,8}$/.test(query)) {
+        if (/^(?:DE|F|US)[0-9]{6,8}$/.test(query)) {
             targetUrl = getRallyUrl(query);
         }
         else if (words.length >= 2) {
