f0d27fbf54e5a80d3f6f51d4aa9e3aa538aa4084
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-04-11 13:49:33 +0300

    wikipedia: Integrated with WikiPedia

--- scripts/main.js
+++ scripts/main.js
@@ -25,6 +25,7 @@ function getProtonDbUrl(query) { return "https://protondb.com/search?q=" + encod
 function getRallyUrl(query) { return "https://rally1.rallydev.com/#/search?keywords=" + encodeURIComponent(query); }
 function getSteamDbUrl(query) { return "https://steamdb.info/search/?a=all&q=" + encodeURIComponent(query); }
 function getUespUrl(query) { return "https://en.uesp.net/wiki/Special:Search?search=" + encodeURIComponent(query); }
+function getWikiPediaUrl(query) { return "https://ro.wikipedia.org/w/index.php?search=" + encodeURIComponent(query + " Torrent"); }
 function getYandexTorrentsUrl(query) { return "https://yandex.com/search/?text=" + encodeURIComponent(query + " Torrent") }
 function getYouTubeUrl(query) { return "https://yewtu.be/search?q=" + encodeURIComponent(query); }
 
@@ -116,6 +117,8 @@ function runSearch(rawQuery) {
                 targetUrl = getSteamDbUrl(words.filter(w => w.toLowerCase() !== "steamdb").join(" "));
             } else if (words.some(w => w.toLowerCase() === "uesp")) {
                 targetUrl = getUespUrl(words.filter(w => w.toLowerCase() !== "uesp").join(" "));
+            } else if (words.some(w => w.toLowerCase() === "wikipedia")) {
+                targetUrl = getWikiPediaUrl(words.filter(w => w.toLowerCase() !== "wikipedia").join(" "));
             } else if (words.some(w => w.toLowerCase() === "youtube")) {
                 targetUrl = getYouTubeUrl(words.filter(w => w.toLowerCase() !== "youtube").join(" "));
             } else {
