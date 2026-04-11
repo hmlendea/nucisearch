3a11d59e96550658424d056a128b287763f67b38
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-04-11 22:38:04 +0300

    lidl: Integrated with Lidl

--- scripts/main.js
+++ scripts/main.js
@@ -22,6 +22,7 @@ function getGoogleMapsUrl(query) { return "https://google.ro/maps/search/" + enc
 function getIkeaUrl(query) { return "https://ikea.com/ro/ro/search/?q=" + encodeURIComponent(query); }
 function getImdbUrl(query) { return "https://libremdb.iket.me/find?q=" + encodeURIComponent(query); }
 function getJyskUrl(query) { return "https://jysk.ro/search?query=" + encodeURIComponent(query); }
+function getLidlUrl(query) { return "https://lidl.ro/q/search?q=" + encodeURIComponent(query); }
 function getMinecraftWikiUrl(query) { return "https://minecraft.wiki/?search=" + encodeURIComponent(query); }
 function getOlxUrl(query) { return "https://olx.ro/d/oferte/q-" + encodeURIComponent(query); }
 function getPcGarageUrl(query) { return "https://pcgarage.ro/cauta/" + encodeURIComponent(query); }
@@ -118,6 +119,8 @@ function runSearch(rawQuery) {
                 targetUrl = getImdbUrl(words.filter(w => w.toLowerCase() !== "imdb").join(" "));
             } else if (words.some(w => w.toLowerCase() === "jysk")) {
                 targetUrl = getJyskUrl(words.filter(w => w.toLowerCase() !== "jysk").join(" "));
+            } else if (words.some(w => w.toLowerCase() === "lidl")) {
+                targetUrl = getLidlUrl(words.filter(w => w.toLowerCase() !== "lidl").join(" "));
             } else if (query.toLowerCase().includes("mc wiki") ||
                        query.toLowerCase().includes("minecraft wiki")) {
                 targetUrl = getMinecraftWikiUrl(query
