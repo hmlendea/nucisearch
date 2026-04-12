8465ba5518584de3757114b3f125b404811e92f1
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-04-12 13:32:57 +0300

    audible: Integrated with Audible

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,7 @@ function getAliExpressUrl(query) {
 function getAltexUrl(query) { return "https://altex.ro/cauta/?q=" + encodeURIComponent(query); }
 function getArchWikiUrl(query) { return "https://wiki.archlinux.org/index.php?search=" + encodeURIComponent(query); }
 function getAuchanUrl(query) { return "https://auchan.ro/" + encodeURIComponent(query); }
+function getAudibleUrl(query) { return "https://audible.com/search?advsearchKeywords=" + encodeURIComponent(query); }
 function getCarturestiUrl(query) { return "https://carturesti.ro/product/search/" + encodeURIComponent(query); }
 function getDecathlonUrl(query) { return "https://decathlon.ro/search?Ntt=" + encodeURIComponent(query); }
 function getDexOnlineUrl(query) { return "https://dexonline.ro/definitie/" + encodeURIComponent(query); }
@@ -125,6 +126,8 @@ function runSearch(rawQuery) {
                 targetUrl = getArchWikiUrl(query.replace(/arch wiki/gi, "").trim());
             } else if (words.some(w => w.toLowerCase() === "auchan")) {
                 targetUrl = getAuchanUrl(words.filter(w => w.toLowerCase() !== "auchan").join(" "));
+            } else if (words.some(w => w.toLowerCase() === "audible")) {
+                targetUrl = getAudibleUrl(words.filter(w => w.toLowerCase() !== "audible").join(" "));
             } else if (words.some(w => w.toLowerCase() === "carturesti")) {
                 targetUrl = getCarturestiUrl(words.filter(w => w.toLowerCase() !== "carturesti").join(" "));
             } else if (words.some(w => w.toLowerCase() === "decathlon")) {
